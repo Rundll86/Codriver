0aea028997a7dc507e53452f3556e5f02d4d8e4e
daviddang <87896813+dangsiyuandavid@users.noreply.github.com>
2024-06-27 21:18:35 +0800

    Change key back to Keycode
    because it doesn't work

--- preload.js
+++ preload.js
@@ -1,15 +1,15 @@
 const { ipcRenderer, contextBridge } = require("type-electron");
 window.addEventListener("keydown", e => {
-    if (e.key === "F12" || e.key == 16) {
+    if (e.key === "F12" || e.keyCode == 16) {
         ipcRenderer.send("toggle-devtool");//Shift_L
     };
-    if (e.key === "F11" || e.key == 27) { 
+    if (e.key === "F11" || e.keyCode == 27) { 
         ipcRenderer.send("close");// esc + esc
     };
     if (e.key === "F10" ) {
         ipcRenderer.send("backai");
     };
-    if (e.key === "F5" || e.key == 17 ) {
+    if (e.key === "F5" || e.keyCode == 17 ) {
         window.location.reload();//Control_L
     };
 });
